73148b6781b0b11f08dbdd53ddb728a788cab34c
Hamza <hamzafarooq49ml@gmail.com>
2026-04-17 05:46:39 +0500

    CRUD operations

---
+++ fsCrud.js
@@ -0,0 +1,25 @@
+const { log } = require("console");
+const fs = require("fs");
+
+// fs.writeFileSync("text/test.txt", "Hello, World!");
+
+// fs.unlinkSync("text/asdasdasdsad.txt");
+
+// const fileData = fs.readFileSync("text/test.txt", "utf-8");
+// console.log(fileData);
+
+// fs.appendFileSync("text/test.txt", " my homie");
+
+// log(process.argv[2], process.argv[3]);
+
+const operation = process.argv[2];
+if (operation === "create") {
+  fs.writeFileSync("text/test.txt", "Hello, World!");
+} else if (operation === "delete") {
+  fs.unlinkSync("text/test.txt");
+} else if (operation === "read") {
+  const fileData = fs.readFileSync("text/test.txt", "utf-8");
+  console.log(fileData);
+} else if (operation === "append") {
+  fs.appendFileSync("text/test.txt", " my homie");
+}
